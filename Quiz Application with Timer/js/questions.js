d81071ee234f7a56f1dac7b57d30646ac8af13e1
Sambuddha Koner <93607635+Sambuddhakoner@users.noreply.github.com>
2022-10-14 22:19:29 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the smallest ocean in the World?",
+    answer: "Arctic",
+    options: [
+      "Indian",
+      "Pacific",
+      "Atlactic",
+      "Arctic"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country gifted the 'Statue of Liberty' to USA in 1886?",
+    answer: "France",
+    options: [
+      "Canada",
+      "Brazil",
+      "France",
+      "England"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country is also known as the 'Land of Tulips?",
+    answer: "Netherland",
+    options: [
+      "Netherland",
+      "Poland",
+      "Venice",
+      "Denmark"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Dead Sea is located between which two countries",
+    answer: "Jordan and Isreal",
+    options: [
+      "Jordan and Sudan",
+      "Jordan and Isreal",
+      "Turkey and UAE",
+      "UAE and Egypt"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which continent has highest number of countries?",
+    answer: "Africa",
+    options: [
+      "Asia",
+      "Europe",
+      "North America",
+      "Africa"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which one is the biggest island in the world?",
+    answer: "Greenland",
+    options: [
+      "Borneo",
+      "Finland",
+      "Sumatra",
+      "Greenland"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which one is the largest tropical rain forest in the World?",
+    answer: "Amazon",
+    options: [
+      "Amazon",
+      "Southeast Asian Rain Forest",
+      "Bosawas",
+      "Daintree Rain Forest"
+    ]
+  },
+];
